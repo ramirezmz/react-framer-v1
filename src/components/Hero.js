2be18bbce37f7db9a65822e2b3ec7c6890c1b61f
ramirezmz <roberto.paolo@dremio.com>
2022-08-11 08:56:35 -0300

    feat: add framer style on button and images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -44,7 +44,7 @@ const ColumnLeft = styled.div`
     line-height: 1.1;
   }
 `
-const Button = styled.button`
+const Button = styled(motion.button)`
   padding: 1rem 3rem;
   font-size: 1rem;
   border: 2px solid #fff;
@@ -54,7 +54,7 @@ const Button = styled.button`
   background: transparent;
   color: #fff;
 `
-const Image = styled.img`
+const Image = styled(motion.img)`
   position: absolute;
   width: 100%;
   height: 100%;
@@ -110,13 +110,55 @@ const Hero = () => {
               animate= 'visible'
               transition={{ duration: 1 }}
             >Journey to the unknown</motion.p>
-            <Button>Get started</Button>
+            <Button
+              whileHover={{ scale: 1.05}}
+              whileTap={{ scale: 0.95,
+                backgroundColor:"#67F6E7",
+                border: "none",
+                color: "#000"}}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, 
+                transition:{ duration: 1.5 }}}
+
+            >Get started</Button>
           </ColumnLeft>
           <ColumnRight>
-            <Image src={PlanetOne} alt='planet'/>
-            <Image src={PlanetTwo} alt='planet-two'/>
-            <Image src={PlanetThree} alt='planet-three'/>
-            <Image src={PlanetFour} alt='planet-four'/>
+            <Image 
+              src={PlanetOne} 
+              alt='planet'
+              whileTap={{ scale: 0.9 }}
+              drag={true}
+              dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50}}
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0, transition: { duration: 1} }}
+              />
+            <Image 
+              src={PlanetTwo}
+              alt='planet-two'
+              whileTap={{ scale: 0.6 }}
+              drag={true}
+              dragConstraints={{ left: 50, right: 0, top: 0, bottom: 50}}
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0, transition: { duration: 1} }}
+              />
+            <Image 
+              src={PlanetThree}
+              alt='planet-three'
+              whileTap={{ scale: 0.8 }}
+              drag={true}
+              dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50}}
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0, transition: { duration: 1} }}
+              />
+            <Image 
+              src={PlanetFour}
+              alt='planet-four'
+              whileTap={{ scale: 0.9 }}
+              drag={true}
+              dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0}}
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0, transition: { duration: 1} }}
+              />
         </ColumnRight>
         </Container>
       </Section>
